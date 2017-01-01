bc33aba47229e5c18294549f1d78be94fecfc3fc
Ben Paddock <pads@thisispads.me.uk>
2017-01-01 16:19:11 +0000

    Add home page placeholder

---
+++ apps/web/controllers/home/index.rb
@@ -0,0 +1,12 @@
+module Web
+  module Controllers
+    module Home
+      # Home page action
+      class Index
+        include Web::Action
+
+        def call(params); end
+      end
+    end
+  end
+end
---
+++ apps/web/views/home/index.rb
@@ -0,0 +1,10 @@
+module Web
+  module Views
+    module Home
+      # Home page view
+      class Index
+        include Web::View
+      end
+    end
+  end
+end
